8218a2e2483c9f5736f96cb887248dde9433c033
JustANull <reid.levenick@gmail.com>
2015-04-01 19:07:09 -0500

    WebsiteInfo working on v3 api

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,9 @@
 extern crate hyper;
 extern crate irc;
 extern crate regex;
+extern crate rustc_serialize as serialize;
 
 mod bot {
-    use irc::client::data::{Command, Config, Message};
-    use irc::client::server::{IrcServer, NetIrcServer, Server};
-    use irc::client::server::utils::ServerExt;
-    use std::io::Error;
-
     pub mod util {
         use hyper::client::{Client, IntoBody};
         use hyper::status::StatusCode;
@@ -73,22 +69,105 @@ mod bot {
     pub mod websiteinfo {
         use irc::client::data::{Command, Message};
         use regex::Regex;
-        //use std::collections::HashMap;
+        use serialize::json;
+        use std::collections::HashMap;
+        use std::collections::hash_map::Entry;
         use std::io::{Error, ErrorKind};
         use super::Subscriber;
         use super::util::{collect_errors, get_reply_target, http_get};
 
-        pub struct WebsiteInfo;
+        pub struct WebsiteInfo {
+            gapi_key: String,
+            xkcd_cache: HashMap<String, String>,
+            yt_cache: HashMap<String, String>
+        }
 
-        fn handle_yt<'a>(this: &mut WebsiteInfo, id: &str) -> Result<String, Error> {
-            Ok("".to_string())
+        fn yt_parse_time(s: &str) -> String {
+            let mut in_time = false;
+            let mut start_idx: Option<usize> = None;
+            let mut prev_char: char = '\u{0000}';
+            let mut length = String::new();
+
+            for (idx, c) in s.chars().enumerate() {
+                match c {
+                    '0' ... '9' => {prev_char = c; continue}
+                    'P' => start_idx = Some(idx + 1),
+                    'T' => {
+                        start_idx = Some(idx + 1);
+                        in_time = true
+                    },
+                    _ => {
+                        if length.len() > 0 {
+                            length.push_str(", ");
+                        }
+
+                        length.push_str(&s[start_idx.unwrap() .. idx]);
+                        length.push(' ');
+                        length.push_str(match c {
+                            'Y' => "year",
+                            'M' => if in_time {"minute"} else {"month"},
+                            'D' => "day",
+                            'H' => "hour",
+                            'S' => "second",
+                            _ => panic!("Unexpected character in ISO 8601 duration string")
+                        });
+
+                        if idx - start_idx.unwrap() > 1 || prev_char != '1' {
+                            length.push('s');
+                        }
+
+                        start_idx = Some(idx + 1);
+                    }
+                }
+            }
+
+            length
+        }
+        fn yt_handler(this: &mut WebsiteInfo, id: &str) -> Result<String, Error> {
+            Ok(match this.yt_cache.entry(id.to_string()) {
+                Entry::Occupied(entry) => entry.get().clone(),
+                Entry::Vacant(entry) => {
+                    let url = format!("https://www.googleapis.com/youtube/v3/videos?id={}&key={}&part=snippet,contentDetails&fields=items(snippet/title,snippet/channelTitle,contentDetails/duration)", id, this.gapi_key);
+
+                    let resp = &try!(http_get(&url))[..];
+                    let resp_json = match json::Json::from_str(resp) {
+                        Ok(resp_json) => resp_json,
+                        Err(e) => return Err(Error::new(ErrorKind::Other, "JSON Parse", Some(format!("{:?}", e))))
+                    };
+
+                    // I chose to panic here on any errors, since if we get a response from Youtube
+                    // it should be of the correct form - their API shouldn't change underneath us.
+                    let ref items = resp_json
+                        .find("items").expect("Youtube API error - could not find 'items'")
+                        .as_array().expect("Youtube API error - 'items' was not an array")[0];
+                    let snippet = items
+                        .find("snippet").expect("Youtube API error - could not find 'snippet'");
+                    let title = snippet
+                        .find("title").expect("Youtube API error - could not find 'title'")
+                        .as_string().expect("Youtube API error - 'title' was not a string");
+                    let channel_title = snippet
+                        .find("channelTitle").expect("Youtube API error - could not find 'channelTitle'")
+                        .as_string().expect("Youtube API error - 'channelTitle' was not a string");
+                    let duration = items
+                        .find("contentDetails").expect("Youtube API error - could not find 'contentDetails'")
+                        .find("duration").expect("Youtube API error - could not find 'duration'")
+                        .as_string().expect("Youtube API error - 'duration' was not a string");
+
+                    let res = format!("\"{}\" by {} ({})", title, channel_title, yt_parse_time(duration));
+                    entry.insert(res).clone()
+                }
+            })
         }
         static URL_RES: &'static [(Regex, fn(&mut WebsiteInfo, &str) -> Result<String, Error>)] =
-            &[(regex!(r"(?:(?:youtube\.com/watch\?\S*?v=)|(?:youtu\.be/))([\w-]+)"), handle_yt)];
+            &[(regex!(r"(?:(?:youtube\.com/watch\?\S*?v=)|(?:youtu\.be/))([\w-]+)"), yt_handler)];
 
         impl WebsiteInfo {
-            pub fn new() -> WebsiteInfo {
-                WebsiteInfo
+            pub fn new(gapi_key: &str) -> WebsiteInfo {
+                WebsiteInfo {
+                    gapi_key: gapi_key.to_string(),
+                    xkcd_cache: HashMap::new(),
+                    yt_cache: HashMap::new()
+                }
             }
         }
 
@@ -123,6 +202,11 @@ mod bot {
             }
         }
     }
+
+    use irc::client::data::{Command, Config, Message};
+    use irc::client::server::{IrcServer, NetIrcServer, Server};
+    use irc::client::server::utils::ServerExt;
+    use std::io::Error;
 
     pub struct Bot<'a> {
         server: NetIrcServer,
@@ -188,9 +272,16 @@ mod bot {
 use bot::Bot;
 use bot::websiteinfo::WebsiteInfo;
 use irc::client::data::Config;
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
 
 fn main() {
-    let mut websiteinfo = WebsiteInfo::new();
+    let mut websiteinfo = WebsiteInfo::new(&File::open(Path::new("gapi_key.dat"))
+                                           .ok().expect("gapi_key.dat should be on the path")
+                                           .chars()
+                                           .map(|c| c.ok().expect("gapi_key.dat should be UTF-8"))
+                                           .collect::<String>());
 
     let mut bot = Bot::new(Config::load_utf8("config.json").unwrap()).unwrap();
 
